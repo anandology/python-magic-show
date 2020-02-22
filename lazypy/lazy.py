d5c2f0c872a55d765d78f7033599060058c8ca02
Anand Chitipothu <anandology@gmail.com>
2020-02-23 00:19:24 +0530

    Added support for lazy sequences to lazy.py

--- lazypy/lazy.py
+++ lazypy/lazy.py
@@ -43,6 +43,9 @@ class Promise:
 
     The `resolve` method returns the value of this promise.
     """
+    def __init__(self, env):
+        self.env = env
+
     def __add__(self, x): return Expr(self.env, operator.__add__, self, x)
     def __mul__(self, x): return Expr(self.env, operator.__mul__, self, x)
     def __sub__(self, x): return Expr(self.env, operator.__sub__, self, x)
@@ -52,6 +55,9 @@ class Promise:
     def __rmul__(self, x): return Expr(self.env, operator.__mul__, x, self)
     def __rsub__(self, x): return Expr(self.env, operator.__sub__, x, self)
     def __rdiv__(self, x): return Expr(self.env, operator.__div__, x, self)
+
+    def __rshift__(self, x): return Iter(self.env, self, x)
+    def __rrshift__(self, x): return Iter(self.env, x, self)
 
     def resolve(self):
         raise NotImplementedError()
@@ -67,17 +73,20 @@ class Symbol(Promise):
     when the value is accessed.
     """
     def __init__(self, env, name):
-        self.env = env
+        super().__init__(env)
         self.name = name
 
     def resolve(self):
         return self.env.resolve(self.name)
 
+    def _repr(self):
+        return "`{}".format(self.name)
+
 class Expr(Promise):
     """Expression on a symbol.
     """
     def __init__(self, env, op, left, right):
-        self.env = env
+        super().__init__(env)
         self.op = op
         self.left = left
         self.right= right
@@ -85,24 +94,102 @@ class Expr(Promise):
     def resolve(self):
         return self.op(resolve(self.left), resolve(self.right))
 
+    def _repr(self):
+        op = {
+            operator.__add__: '+',
+            operator.__sub__: '-',
+            operator.__mul__: '*'
+        }[self.op]
+
+        return "({} {} {})".format(_repr(self.left), op, _repr(self.right))
+
+class LazyFunction:
+    def __init__(self, env, func):
+        self.env = env
+        self.func = func
+
+    def __call__(self, *a, **kw):
+        return LazyResult(self.env, self.func, a, kw)
+
+class LazyResult(Promise):
+    def __init__(self, env, func, a, kw):
+        super().__init__(env)
+        self.func = func
+        self.a = a
+        self.kw = kw
+
+    def resolve(self):
+        a = resolve(self.a)
+        kw = resolve(self.kw)
+        return self.func(*a, *kw)
+
+    def _repr(self):
+        return "<LazyResult {} {} {}>".format(self.func, self.a, self.kw)
+
+class Iter:
+    def __init__(self, env, head, tail):
+        self.env = env
+        self._head = head
+        self._tail = tail
+
+    def __add__(self, other):
+        def add():
+            nonlocal other
+            other = resolve(other)
+            head = resolve(self._head) + resolve(other._head)
+            return Iter(self.env, head, self._tail + other._tail)
+        return LazyResult(self.env, add, (), {})
+
+    def __iter__(self):
+        yield resolve(self._head)
+        yield from resolve(self._tail)
+
+    def resolve(self):
+        return self
+
+    def _repr(self):
+        return "[{} {}]".format(_repr(self._head), _repr(self._tail))
+
+    __repr__ = _repr
+
 def resolve(x):
     if isinstance(x, (list, tuple)):
         return [resolve(v) for v in x]
     elif isinstance(x, dict):
         return {resolve(k): resolve(v) for k, v in x.items()}
+    elif isinstance(x, Iter):
+        return x
     elif isinstance(x, Promise):
         return resolve(x.resolve())
     else:
         return x
 
-def lazy_exec(code, env=None):
-    if env is None:
-        env = LazyEnv()
-    exec(code, env, env)
+def _repr(x):
+    if isinstance(x, Promise):
+        return x._repr()
+    else:
+        return repr(x)
+
+def take(n, values):
+    values = iter(resolve(values))
+    return [next(values) for i in range(n)]
+
+def tail(values):
+    values = resolve(values)
+    if isinstance(values, Iter):
+        return values._tail
+    else:
+        raise ValueError("Not an Iter")
+
+ENV = LazyEnv()
+BUILTINS['take'] = LazyFunction(ENV, take)
+BUILTINS['tail'] = LazyFunction(ENV, tail)
+
+def lazy_exec(code):
+    exec(code, ENV, ENV)
 
 def lazy_eval(code):
-    env = LazyEnv()
-    return eval(code, env, env)
+    return eval(code, ENV, ENV)
 
 def main():
     if len(sys.argv) > 1:
